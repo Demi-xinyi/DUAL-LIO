a4953fe76688ea6340e2f663038de8c20d9e220a
Demi-xinyi <73590465+Demi-xinyi@users.noreply.github.com>
2025-07-11 18:33:14 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,30 +1,24 @@
-%% 初始化环境
 clear; close all; clc;
 addpath(genpath('./src'));
 addpath('./data');
 
 
-%% 加载配置
 config = config_default();
 
 config.startTime = 0;
 config.stopTime = 40;
 
-% 设置数据路径
 [filePathL, filePathR, truthPath] = deal( ...
     'data/fl', ...
     'data/rr', ...
     'data/truth.csv');
 
-%% 加载数据
 [timeL, accL, gyrL, timeR, accR, gyrR, groundtruth] = ...
     load_dataset(filePathL, filePathR, truthPath, config);
 
 
-%% 运行
 pos = run_lio(config,timeL, accL, gyrL, timeR, accR, gyrR);
 
-%% 绘图对比轨迹
 
 
 figure;
